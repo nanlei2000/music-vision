01c6be998965238b809d8dd180c756a18a726b65
nanlei <36439285+nanlei2000@users.noreply.github.com>
2019-08-10 22:59:20 +0800

    chore: 添加 build 目录

---
+++ build/precache-manifest.f888d1c019de02da5a456f7332b6d1f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fda1aae30c462bffaa5557f3bf31950",
+    "url": "/index.html"
+  },
+  {
+    "revision": "165b8a91e433d2daadb6",
+    "url": "/static/css/main.1d377785.chunk.css"
+  },
+  {
+    "revision": "fffc49decc89d9354d7a",
+    "url": "/static/js/2.5d5225fd.chunk.js"
+  },
+  {
+    "revision": "165b8a91e433d2daadb6",
+    "url": "/static/js/main.c9bead56.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
